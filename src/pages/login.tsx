651d06df6230cd25cc7ca224de1e7941f335b540
Cade Cuddy <cadecuddy@gmail.com>
2022-12-13 02:39:45 -0500

    correct login loading render

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,11 +4,16 @@ import { FaDiscord, FaGoogle } from "react-icons/fa";
 import HeaderLayout from "../layouts/HeaderLayout";
 import Footer from "../components/Footer";
 import Router from "next/router";
+import Loading from "../components/Loading";
 
 export default function login() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   if (session) {
     Router.push("/");
+    return <Loading />;
+  }
+  if (status === "loading") {
+    return <Loading />;
   }
 
   const handleSignIn = async (provider: string) => {
@@ -20,57 +25,41 @@ export default function login() {
 
   return (
     <>
-      <div className="mt-12 flex flex-col items-center justify-center py-2">
-        <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-          <h1 className="text-6xl font-bold">
-            Welcome to{" "}
-            <a href="/" className="text-[#72cea6]">
-              GeoSleuth
-            </a>
-          </h1>
-          <p className="mt-3 text-2xl">
-            Sign in with one of our providers below.
-          </p>
-          <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-            <button
-              onClick={() => handleSignIn("discord")}
-              className="borderborder-transparent mt-6 w-96 rounded-xl bg-transparent p-6 text-left shadow-2xl shadow-slate-800 transition-transform duration-300 ease-out hover:cursor-pointer hover:bg-[#7289da] hover:text-white hover:shadow-neutral-800 focus:outline-none hover:md:scale-105"
-            >
-              <FaDiscord className="mr-4 inline-block h-8 w-8" />
-              <span className="text-2xl font-bold">Sign in with Discord</span>
-            </button>
-          </div>
-          <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-            <button
-              onClick={() => handleSignIn("google")}
-              className="borderborder-transparent mt-6 w-96 rounded-xl bg-transparent p-6 text-left shadow-2xl shadow-slate-800 transition-transform duration-300 ease-out hover:cursor-pointer hover:bg-[#ea4335] hover:text-white hover:shadow-neutral-800 focus:outline-none hover:md:scale-105"
-            >
-              <FaGoogle className="mr-4 inline-block h-8 w-8" />
-              <span className="text-2xl font-bold">Sign in with Google</span>
-            </button>
-          </div>
-        </main>
-      </div>
+      {!session && (
+        <div className="mt-12 flex flex-col items-center justify-center py-2">
+          <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+            <h1 className="text-6xl font-bold">
+              Welcome to{" "}
+              <a href="/" className="text-[#72cea6]">
+                GeoSleuth
+              </a>
+            </h1>
+            <p className="mt-3 text-2xl">
+              Sign in with one of our providers below.
+            </p>
+            <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+              <button
+                onClick={() => handleSignIn("discord")}
+                className="borderborder-transparent mt-6 w-96 rounded-xl bg-transparent p-6 text-left shadow-2xl shadow-slate-800 transition-transform duration-300 ease-out hover:cursor-pointer hover:bg-[#7289da] hover:text-white hover:shadow-neutral-800 focus:outline-none hover:md:scale-105"
+              >
+                <FaDiscord className="mr-4 inline-block h-8 w-8" />
+                <span className="text-2xl font-bold">Sign in with Discord</span>
+              </button>
+            </div>
+            <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+              <button
+                onClick={() => handleSignIn("google")}
+                className="borderborder-transparent mt-6 w-96 rounded-xl bg-transparent p-6 text-left shadow-2xl shadow-slate-800 transition-transform duration-300 ease-out hover:cursor-pointer hover:bg-[#ea4335] hover:text-white hover:shadow-neutral-800 focus:outline-none hover:md:scale-105"
+              >
+                <FaGoogle className="mr-4 inline-block h-8 w-8" />
+                <span className="text-2xl font-bold">Sign in with Google</span>
+              </button>
+            </div>
+          </main>
+        </div>
+      )}
     </>
   );
 }
 
-login.getLayout = (page: ReactElement) => (
-  <div>
-    <HeaderLayout>{page}</HeaderLayout>
-    <div
-      className="
-      fixed bottom-0
-      flex
-      w-full
-      items-center
-      justify-center
-      text-sm
-      font-bold
-      text-neutral-100
-      "
-    >
-      <Footer />
-    </div>
-  </div>
-);
+login.getLayout = (page: ReactElement) => <HeaderLayout>{page}</HeaderLayout>;
